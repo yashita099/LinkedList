1af8d8b3f146ac791a870f383c4a16daf02692b7
Yashita Gaur <yashitagaur099@gmail.com>
2025-07-30 13:08:10 +0530

    Create Reorder list .java

---
+++ Reorder list .java
@@ -0,0 +1,26 @@
+public void reorderList(ListNode head) {
+        if (head == null || head.next == null) {
+            return;
+        }
+
+        ListNode mid = middleNode(head);
+
+        ListNode hs = reverseList(mid);
+        ListNode hf = head;
+
+        // rearrange
+        while (hf != null && hs != null) {
+            ListNode temp = hf.next;
+            hf.next = hs;
+            hf = temp;
+
+            temp = hs.next;
+            hs.next = hf;
+            hs = temp;
+        }
+
+        // next of tail to null
+        if (hf != null) {
+            hf.next = null;
+        }
+    }
