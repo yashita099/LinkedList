8ee900ddb16a702361b8a7ec8c2658650e6d0750
Yashita Gaur <yashitagaur099@gmail.com>
2025-07-30 13:07:21 +0530

    Create Palindrome LL .java

---
+++ Palindrome LL .java
@@ -0,0 +1,17 @@
+ public boolean isPalindrome(ListNode head) {
+        ListNode mid = middleNode(head);
+        ListNode headSecond = reverseList(mid);
+        ListNode rereverseHead = headSecond;
+
+        // compare both the halves
+        while (head != null && headSecond != null) {
+            if (head.val != headSecond.val) {
+                break;
+            }
+            head = head.next;
+            headSecond = headSecond.next;
+        }
+        reverseList(rereverseHead);
+
+        return head == null || headSecond == null;
+    }
